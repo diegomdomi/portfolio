7953171369cba10205727028b3b92c0e6c4efd40
diegomdomi <diegomdomi@gmail.com>
2023-04-29 13:40:09 +0200

    adding spanish cv

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import CV from '../../assets/cv-DiegoDominguez.pdf';
+import CVE from '../../assets/cv_DiegoDominguez.pdf';
 
 const CTA = () => {
   return (
     <div className="cta">
+      <a href={CVE} download className="btn">
+        Download CV Eng.
+      </a>
       <a href={CV} download className="btn">
-        Download CV
+        Download CV Esp.
       </a>
       <a href="#contact" className="btn btn-primary">
         Let's talk
